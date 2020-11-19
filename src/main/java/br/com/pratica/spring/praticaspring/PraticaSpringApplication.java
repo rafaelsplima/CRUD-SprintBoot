47581baa91ea637b831225939c7760ce83f1f71f
Rafael-Lima <rafaelsplima@gmail.com>
2020-11-18 23:05:51 -0300

    setup project

---
+++ src/main/java/br/com/pratica/spring/praticaspring/PraticaSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.pratica.spring.praticaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pratica/spring/praticaspring/PraticaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pratica.spring.praticaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
